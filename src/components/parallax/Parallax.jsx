185332c2384b04c817ea6b0927d52fb720b2a4f3
OMRAKESHMITTAL <ommittal0104@gmail.com>
2024-11-23 17:48:57 +0530

    mycommit

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -37,9 +37,9 @@ const Parallax =({type} ) =>{
         >
             <motion.h1 style={{y: yBg}}>  {type === "services" ? "About Me!" : ""}</motion.h1>
             <motion.h2 style={{y: yBg}}>  {type !== "services" ? "Here’s What I’ve Been Working On" : ""}</motion.h2>
-            <motion.h3  
+            <motion.h3
     style={{x: yB}}>
-    {type === "services" ? "I’m a passionate and dedicated B.Tech graduate in Computer Science Engineering from Delhi Technical Campus, affiliated with Guru Gobind Singh Indraprastha University. Currently, I’m pursuing an MBA at MAIT (Maharaja Agrasen Institute of Technology), also under Guru Gobind Singh Indraprastha University.\n" +
+    {type === "services" ? "I’m a passionate and dedicated B.Tech graduate in Computer Science Engineering from Delhi Technical Campus, affiliated with Guru Gobind Singh Indraprastha University. Currently, I’m pursuing an MBA at MAIT (Maharaja Agrasen Institute of Technology), also under Guru Gobind Singh Indraprastha University.\n"+
     "I have a keen interest in Ethical Hacking, Cloud Computing, and Cybersecurity. My technical journey has equipped me with strong analytical and problem-solving skills, as well as a deep understanding of emerging technologies in the digital security domain.\n" +
     "Beyond academics, I’m a gamer and an avid traveler who enjoys exploring new places and cultures, which fuels my creativity and broadens my perspective.\n" +
     "I’m always eager to tackle complex challenges, share innovative ideas, and collaborate with others to turn possibilities into reality. If you’re interested in connecting or discussing, feel free to reach out. Let’s create something amazing together!" : ""}
